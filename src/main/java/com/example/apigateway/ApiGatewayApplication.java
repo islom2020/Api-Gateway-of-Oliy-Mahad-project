31d5186ca286541ef9cb6aaa12191bca0b55d93f
Akhror_Mamasodirov <axror19977@gmail.com>
2022-05-27 11:16:06 +0500

    blyat

--- src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,15 +1,32 @@
 package com.example.apigateway;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableEurekaClient
+//@CrossOrigin(origins = "*",allowedHeaders = "*")
+@RestController
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
+
+    @GetMapping("/test")
+    public String test() {
+        return "TEST";
+    }
+
+
 
 }
---
+++ src/main/java/com/example/apigateway/config/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class WebSecurityConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+                .maxAge(-1)   // add maxAge
+                .allowCredentials(false);
+    }
+
+
+
+}
